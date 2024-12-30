b5865429ec46ec498a924412302d7a9078f21e38
Jean Olmedillo <jeanolmedillo@outlook.com>
2024-12-30 00:39:43 -0300

    fix: env

--- src/models/models.py
+++ src/models/models.py
@@ -19,7 +19,7 @@ supabase: Client = create_client(url, key)
 
 
 # DATABASE_URL = os.getenv("DATABASE_URL", "default.db")
-engine = create_engine("sqlite:///database.db", echo=True)
+engine = create_engine("sqlite:///database.db", echo=False)
 
 Base = declarative_base()
 
